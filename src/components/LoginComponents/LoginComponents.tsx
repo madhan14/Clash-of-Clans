d25f4e499ac0bc2a7d5ebb62956cbc68ac7e33cd
GR <madhankumar05072000@protonmail.com>
2022-12-26 12:32:26 +0530

    :zap: Login page

---
+++ src/components/LoginComponents/LoginComponents.tsx
@@ -0,0 +1,55 @@
+import React from 'react';
+import { Button, Form, Input, Modal } from 'antd';
+
+const LoginComponent: React.FC = () => {
+
+    const onFinish = (values: any) => {
+        console.log(values);
+    };
+    
+    const onFinishFailed = (errorInfo: any) => {
+        console.log(errorInfo);
+    };
+
+    return (
+        <>
+            <Modal title="Login" open={true} footer={ null } closable={ false } >
+                <Form
+                    name='Login'
+                    autoComplete='off'
+                    onFinish={onFinish}
+                    onFinishFailed={onFinishFailed}
+                >
+                    <Form.Item
+                        name='clanID'
+                        label='Clan ID: '
+                        rules={[{ required: true, message: 'This field is required!' }]}
+                    >
+                        <Input />
+                    </Form.Item>
+                    <Form.Item
+                        name='playerID'
+                        label='Player ID: '
+                        rules={[{ required: true, message: 'This field is required!' }]}
+                    >
+                        <Input />
+                    </Form.Item>
+                    <Form.Item
+                        name='apiKey'
+                        label='API Key: '
+                        rules={[{ required: true, message: 'This field is required!' }]}
+                    >
+                        <Input />
+                    </Form.Item>
+                    <Form.Item>
+                        <Button type="primary" htmlType="submit">
+                            Submit
+                        </Button>
+                    </Form.Item>
+                </Form>
+            </Modal>
+        </>
+    );
+}
+
+export default LoginComponent;
